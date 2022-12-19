af1444c13b0113f7bb16693298d1d294cb014b24
Viacheslav Zinovev <Postamentovich@gmail.com>
2022-12-19 19:19:04 +0000

    Sync LeetCode submission - Two Sum (typescript)

--- problems/two_sum/solution.ts
+++ problems/two_sum/solution.ts
@@ -1,12 +1,9 @@
-function twoSum(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            const valueA = nums[i];
-            const valueB = nums[j];
-            if (valueA + valueB === target) {
-                return [i, j]
-            }
-        }
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const firstNumber = nums[i];
+      const secondNumber = nums[j];
+      if (firstNumber + secondNumber === target) return [i, j];
     }
-    return [0, 0];
-};
+  }
+}
