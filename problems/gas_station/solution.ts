bc1898f89a36a84b86760683b1b623e19f07399a
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-01-07 06:42:52 +0000

    Sync LeetCode submission - Gas Station (typescript)

---
+++ problems/gas_station/solution.ts
@@ -0,0 +1,22 @@
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+    mainLoop:
+    for (let i = 0; i < gas.length; i++) {
+        let totalGas = 0;
+        for (let j = 0; j < gas.length; j++) {
+            const currentIndex = i + j >= gas.length ? i+ j - gas.length : i + j;
+            totalGas += gas[currentIndex];
+            const requiredToNext = cost[currentIndex];
+            
+            if (requiredToNext <= totalGas) {
+               totalGas -= requiredToNext; 
+            } else {
+                continue mainLoop;
+                break;
+            }
+        }
+        
+        return i;
+        
+    }
+    return -1;
+};
