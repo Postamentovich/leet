07e836471d050159978ef6b52806892daeb308ee
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-04-17 06:38:30 +0000

    Sync LeetCode submission - Debounce (typescript)

---
+++ problems/debounce/solution.ts
@@ -0,0 +1,21 @@
+type F = (...p: any[]) => any
+
+function debounce(fn: F, t: number): F {
+    let timeout;
+    return function(...args) {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
