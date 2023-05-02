318448e654b9c46a3c40f99aad7112f276c3ecb8
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-05-02 15:43:57 +0000

    Sync LeetCode submission - Binary Search (typescript)

--- problems/binary_search/solution.ts
+++ problems/binary_search/solution.ts
@@ -1,3 +1,21 @@
 function search(nums: number[], target: number): number {
-    return nums.findIndex(num => num === target);
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const index = left + Math.floor((right - left) / 2);
+        const value = nums[index];
+        if (value === target) {
+            return index;
+        }
+
+        if (value < target) {
+            left = index + 1;
+        } else {
+            right = index - 1;
+        }
+    }
+
+    return - 1;
+    
 };
