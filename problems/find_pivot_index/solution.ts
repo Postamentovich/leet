65185101fea72eb66653a958ce9f8fce3c735bc6
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-07-04 01:34:52 +0000

    Sync LeetCode submission - Find Pivot Index (typescript)

--- problems/find_pivot_index/solution.ts
+++ problems/find_pivot_index/solution.ts
@@ -1,18 +1,29 @@
 function pivotIndex(nums: number[]): number {
-    const numsSum = nums.reduce((acc, num) => acc + num, 0);
-    let leftSum = 0;
-    let rightSum = numsSum;
+    const rightSum = {
+        [nums.length - 1]: 0,
+    };
+    const leftSum = {
+        0: 0
+    };
 
-    for (let i = 0; i < nums.length; i++) {
-        const currentNumber = nums[i];
-        rightSum -= currentNumber;
-
-        if (leftSum === rightSum) {
-            return i;
-        }
-
-        leftSum += currentNumber;
+    for (let i = 1; i < nums.length; i++) {
+        const value = nums[i - 1];
+        const previousSum = leftSum[i - 1];
+        leftSum[i] = previousSum + value;
     }
 
-    return -1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        const value = nums[i + 1];
+        const previousSum = rightSum[i + 1];
+        rightSum[i] = previousSum + value
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (rightSum[i] === leftSum[i]) {
+            return i;
+        }
+    }
+
+    return - 1;
 };
