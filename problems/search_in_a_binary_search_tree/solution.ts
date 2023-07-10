71fc434a3a820fb92a32de4f5e0ec14bf6948f67
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-07-10 04:11:04 +0000

    Sync LeetCode submission - Search in a Binary Search Tree (typescript)

---
+++ problems/search_in_a_binary_search_tree/solution.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (!root) {
+        return null;
+    }
+    if (root.val === val) {
+        return root;
+    }
+    if (root.val > val) {
+        return searchBST(root.left, val);
+    }
+    return searchBST(root.right, val);
+};
