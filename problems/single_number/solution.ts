0e6eabfac47b63755f2206bdfc63a18b0449b4b7
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-07-31 13:27:05 +0000

    Sync LeetCode submission - Single Number (typescript)

--- problems/single_number/solution.ts
+++ problems/single_number/solution.ts
@@ -1,21 +1,9 @@
 function singleNumber(nums: number[]): number {
-    while (nums.length) {
-        const value = nums.shift();
-        const dublicateIndex = nums.findIndex(el => el === value);
-        if (dublicateIndex === -1) {
-            return value as number;
-        }
-        nums.splice(dublicateIndex, 1)
+    let result = 0;
+
+    for (const num of nums) {
+        result ^= num;
     }
-    
-    
-//     for (let i = 0; i < nums.length; i++) {
-       
-//         console.log('value', value);
-        
-//         console.log('dublicateIndex', dublicateIndex);
-        
-//         console.log('nums', nums)
-//     }
-    return 0;
+
+    return result;
 };
