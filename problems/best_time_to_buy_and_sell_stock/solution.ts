b3916e908cb4cef6d65c3179c0b917ccfba862c5
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-08-13 13:35:22 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (typescript)

--- problems/best_time_to_buy_and_sell_stock/solution.ts
+++ problems/best_time_to_buy_and_sell_stock/solution.ts
@@ -1,21 +1,20 @@
 function maxProfit(prices: number[]): number {
-  let buyIndex = 0;
-  let sellIndex = 1;
-  let result = 0;
+    let profit = 0;
+    let buy = 0;
+    let sell = 1;
 
-  while (sellIndex < prices.length) {
-    const buyPrice = prices[buyIndex];
-    const sellPrice = prices[sellIndex];
 
-    if (sellPrice > buyPrice) {
-      const profit = sellPrice - buyPrice;
-      result = Math.max(result, profit);
-    } else {
-      buyIndex = sellIndex;
+    while (sell < prices.length) {
+        const currentProfit = prices[sell] - prices[buy];
+
+        if (currentProfit < 0) {
+            buy = sell;
+        } else {
+            profit = Math.max(currentProfit, profit);
+        }
+        
+        sell++;
     }
 
-    sellIndex++;
-  }
-
-  return result;
+    return profit;
 };
