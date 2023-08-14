c24d9593a4ee5038b59c4f6535bf7bcebf2486c2
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-08-14 13:23:08 +0000

    Sync LeetCode submission - Jump Game (typescript)

--- problems/jump_game/solution.ts
+++ problems/jump_game/solution.ts
@@ -1,17 +1,13 @@
 function canJump(nums: number[]): boolean {
     let maxIndex = 0;
-    
+
     for (let i = 0; i < nums.length; i++) {
         if (i > maxIndex) {
             return false;
         }
-        
-        maxIndex = Math.max(maxIndex, i + nums[i]);
-        
-        if (maxIndex >= nums.length - 1) {
-            return true;
-        }
+        const jumpIndex = i + nums[i];
+        maxIndex = Math.max(jumpIndex, maxIndex)
     }
-    
-    return false;
+
+    return true;
 };
