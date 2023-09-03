bafa4e2aba4e26750e70525ae3e5f395576f6c84
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-09-03 14:36:11 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (typescript)

---
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.ts
@@ -0,0 +1,16 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let left = 0;
+    let right = numbers.length - 1;
+
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+};
