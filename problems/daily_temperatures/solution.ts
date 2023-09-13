4aefa0987594303b1977509f80569102ed9edc30
Viacheslav Zinovev <Postamentovich@gmail.com>
2023-09-13 13:06:25 +0000

    Sync LeetCode submission - Daily Temperatures (typescript)

--- problems/daily_temperatures/solution.ts
+++ problems/daily_temperatures/solution.ts
@@ -1,15 +1,19 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-  const result: number[] = [];
+    const result = [];
+    const stack = []; // [val, ind]
 
-  loop1: for (let i = 0; i < temperatures.length; i++) {
-    for (let j = i + 1; j < temperatures.length; j++) {
-      if (temperatures[j] > temperatures[i]) {
-        result.push(j - i);
-        continue loop1;
-      }
+    for (let i = 0; i < temperatures.length; i++) {
+        const value = temperatures[i];
+        while (stack.length && stack[stack.length - 1][0] < value) {
+            const [_, ind] = stack.pop();
+            result[ind] = i - ind;
+        }
+        stack.push([value, i]);
     }
-    result.push(0);
-  }
 
-  return result;
-}
+    stack.forEach(([_, index]) => {
+        result[index] = 0;
+    })
+
+    return result;
+};
